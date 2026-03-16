da6c4ecc372b52250baadda20bf54cf556f3a324
dmmosh <evilsharkeater@gmail.com>
2026-03-16 19:13:12 -0400

    bringing repo to date

--- stocks/server/server.py
+++ stocks/server/server.py
@@ -8,6 +8,8 @@ import subprocess
 import yfinance as yf
 import pandas as pd
 
+import time
+
 '''
 bluetooth stock prices examplee
 make for short time spans, so daytrader
@@ -55,13 +57,42 @@ def relative_volatility_index(df, period=14):
 
     return rvi
 
+
+
+
+# sends the socket
+def send_socket(sock:socket, view:memoryview,chunk_size= 2048):
+    size = len(view) 
+    sock.sendall(size.to_bytes(4,byteorder='big')) 
+    i = 0
+    while(i<size):
+        try:
+            # sendall() is still the safest bet for ensuring delivery
+            i+= client_sock.send(view[i : i + chunk_size])
+            
+        except socket.error as e:
+            print(f"Send failed: {e}")
+            break
+    
+
+
+
 snp500 = pd.read_excel('https://www.ssga.com/us/en/intermediary/etfs/library-content/products/fund-data/etfs/us/holdings-daily-us-en-spy.xlsx', 
                         engine='openpyxl', 
                         skiprows=4,
                         usecols=['Ticker']
-                        ).dropna()['Ticker'].tolist()
+                        ).dropna()['Ticker'].to_numpy()
 
-data = yf.download(tickers=snp500,
+
+
+# for i,num in enumerate(snp500):
+#     print(f"'{num}'",', ', sep='', end='')
+#     if(i%10 == 0):
+#         print()
+    
+snp_i = {value: index for index, value in enumerate(snp500)}
+
+data = yf.download(tickers=list(snp500),
                      period='1d',
                      interval='1m',
                      threads=40
@@ -72,56 +103,43 @@ data = yf.download(tickers=snp500,
 gets the relative volatility index (RVI) with 14 periods
 '''
 #close = pd.concat({t: data[t]["Close"] for t in data}, axis=1)
+rank = np.searchsorted(data['Close'].dropna().columns.values,
+                       snp500, 
+                       sorter=np.argsort(data['Close'].dropna().columns.values)) 
 
+print(data['Close'])
 # RVI , gets the most recent, drops the NaN
 rvi = relative_volatility_index(data['Close']).tail(1).iloc[-1] # turns into series
-rvi = rvi.dropna().sort_values(ascending=False)
-with pd.option_context('display.float_format', '{:.20f}'.format):
-    print(rvi)
+rvi = (rvi.dropna().sort_values(ascending=False).clip(0,99.99) * 100 )
 
+rvi_rank = np.array([snp_i[i] for i in rvi.keys() if i in snp_i]).astype(np.uint16) # actual indeces that will be sent alongside the compressed data
 
-#volatility = ((stocks['High'] - stocks['Close'])/stocks['Price']).T.dropna()
-#print('NUM OF NANS:', volatility.isna().sum())
-#print(volatility)
+rvi = np.ascontiguousarray(rvi,dtype=np.uint16)
+# with pd.option_context('display.float_format', '{:.20f}'.format):
+#     print(rvi) 
 
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-#     print(volatility)
-#print(type(obj.obj.obj))
-#print(obj.obj.size)
+bit_sizes = np.ascontiguousarray([0]*17,dtype=np.uint64)
+for r in rvi:
+    bit_sizes[int(log2(r))+1 if r>0 else 0] +=1
 
-byte = 4
-max_value = pow(256,byte)-1
-n = 10000
-
-type_arr = None
-match(byte):
-    case 1:
-        type_arr = np.uint8
-    case 2:
-        type_arr = np.uint16
-    case 4:
-        type_arr = np.uint32
-    case 8:
-        type_arr = np.uint64
-    case _:
-        raise TypeError('invalid byte size')
-
-values = np.sort(np.random.randint(0,max_value,n,dtype=type_arr))[::-1]
-bit_hash = np.array([0]*(byte*8+1),dtype=np.uint64)
-for v in values:
-    bit_hash[int(log2(v))+1 if v>0 else 0] +=1
-
-obj = bcp_obj(values,bit_hash,type_arr)
-obj.print()
-out = obj.arr()
-
-#obj.print_values()
+    
+rvi_obj = bcp_obj(rvi,
+              bit_sizes,
+              np.uint16
+)    
         
 # CREATE SERVER AND MAKE IT DISCOVERABLE 
 set_bluetooth_discovery(True)
 
 # 2. Create the RFCOMM socket
 # Note: RFCOMM is the standard 'Classic' serial protocol
+
+bmarks = {
+    'uc': 0, #uncompressed
+    'bp':0, # bit compression
+    'bcp': 0 # bit count compression
+}
+
 server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
 try:
@@ -131,57 +149,26 @@ try:
     
     print("Waiting for a client to connect...")
     
+    # establishes connection with the client
     client_sock, client_info = server_sock.accept()
     print(f"Connection established with {client_info}")
     
     
-    # buffer = (c_ubyte * obj.obj.size).from_address(obj.obj.obj) # converts to a unsigned byte array view
-    # view = memoryview(buffer) # creates a memoryview (instead of a bytes object)
-    #print(len(view)) confirmation
-
+    # memory views
+    rvi_view = memoryview(rvi)
+    rvi_obj_view= memoryview((c_ubyte * (rvi_obj.obj.size)).from_address(rvi_obj.obj.obj)),
+    
+    # sends the string array of tickers (index is key)
+    send_socket(client_sock,snp500.astype('S6').tobytes()) # sends the indeces (keys) of snp 500  (only once)
     
     
-    buffer = (c_ubyte * (obj.obj.size)).from_address(obj.obj.obj) # converts to a unsigned byte array view
-    view = memoryview(buffer) # creates a memoryview (instead of a bytes object)
-    
-    chunk_size = 5096 # chunks, n of bytes
-    #n_chunks = obj.obj.size // chunk_size + (obj.obj.size % chunk_size != 0)
-    print(obj.obj.size)
-    # first request will be the n of chunks
-    client_sock.send(obj.obj.size.to_bytes(4,byteorder='big')) 
-    #print(obj.obj.size)
-
-    #client_sock.sendall(view)    
-    
-    i=0
-    while(i<obj.obj.size):
-        try:
-            # sendall() is still the safest bet for ensuring delivery
-            i+= client_sock.send(view[i : i + chunk_size])
-            
-        except socket.error as e:
-            print(f"Send failed: {e}")
-            break
-   
-    
-    client_sock.shutdown(socket.SHUT_WR)
     
     
-    # 3. Simple Echo Loop
-    #while True:
-        #  data = client_sock.recv(1024)
-        # # print(len(data))
-        
-        # if not data:
-        #     print(f"Connection closed by client {client_info}")
-        #     break
-        
-        
-        #print(f"Received: {data.decode('ascii')}")
-        
-        
-        
-        #client_sock.send(b"ACK: " + data)
+    client_sock.shutdown(socket.SHUT_WR )
+    #send_socket(client_sock, rvi_rank) # sends the rvi rank (uncompressed)
+    
+    
+    
 
 except Exception as e:
     print(f"Server Error: {e}")
